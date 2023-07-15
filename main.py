1f269fe1c8e752e68e9a4ae5debd644176ad8d81
NGS Official <123272327+ngs-official@users.noreply.github.com>
2023-07-15 10:54:21 -0400

    Update and rename tokenGrabber.py to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
-# Developed by J0HAN | This is a 100% fake token grabber by the way.
+# Developed by J0HAN | v1.1 | This is a 100% fake token grabber by the way.
 
-import base64, random, string, discord
+import base64, random, string, discord, re, asyncio
 
 try:
     with open("config.txt") as file:
@@ -24,15 +24,15 @@ async def on_message(message):
     if message.content.startswith(f"{prefix}grab"):
         command = message.content.split(" ")
         # Checking if an user ID is inputted
-        if len(command) == 2:
-            # Checking if the user ID is 19 characters
-            if len(command[1]) == 19:
+        if len(command) > 1:
+            # Checking if the user ID is more than or equal to 18 characters
+            if len(command[1]) >= 18:
                 try:
                     # Testing if user ID is a number
                     targetID = int(command[1])
                     pass_test = True
                 except:
-                    message.reply("User IDs shouldn't contain any letters.")
+                    await message.reply("User IDs shouldn't contain any letters.")
                     pass_test = False
 
                 if pass_test == True:
@@ -53,48 +53,54 @@ async def on_message(message):
                         # Just random letters lol
                         token2 = "".join(random.choices(string.ascii_letters, k=5))
                         token3 = "".join(random.choices(string.ascii_letters, k=38))
-                        fake_token = token1 + "." + token2 + "." + token3
+                        target_ftoken = token1 + "." + token2 + "." + token3
 
-                        print(f"User Token: {fake_token}")
+                        print(f"User Token: {target_ftoken}")
 
                         # Embed
+                        target_display = target.display_name
                         target_pfp = target.avatar
                         target_creationDate = target.created_at
                         target_dateCreated = str(target_creationDate).split(" ")
+                        target_flags = target.public_flags.all()
+                        if not target_flags == "[]":
+                            badges = 0
+                            for flag in target_flags:
+                                if badges > 0:
+                                    target_badges = target_badges + ", " + (str(flag).replace("UserFlags.", "").replace("_", " ").title())
+                                else:
+                                    target_badges = str(flag).replace("UserFlags.", "").replace("_", " ").title()
+                                badges += 1
+                        else:
+                            target_badges = "No badges to display."
 
                         tokenEmbed = discord.Embed(title="ACCOUNT FUCKED BY NAHU", description=
-    f"""▸ **Username:**
-    {target}
-    ▸ **User ID:**
-    {targetID}
-    ▸ **Creation Date:**
-    {target_dateCreated[0]}
-    ▸ **Token:**
-    {fake_token}"""
-    , colour=000000)
+f"""▸ **💎 Display Name:**
+{target_display}
+▸ **📄 Username:**
+{target}
+▸ **📝 User ID:**
+{targetID}
+▸ **🛠️ Creation Date:**
+{target_dateCreated[0]}
+▸ **🏅 Badges:**
+{target_badges}
+▸ **👑 Token:**
+{target_ftoken}"""
+                        , colour=000000)
                         tokenEmbed.set_author(name=target, icon_url=target_pfp)
                         tokenEmbed.set_thumbnail(url=target_pfp)
                         tokenEmbed.set_footer(text="Developed by NAHU & JXX")
                         try:
-                            await message.reply(embed=tokenEmbed)
+                            loading = await message.reply(f"Grabbing token of {target}...")
+                            await asyncio.sleep(1.5) # Delay time, set to 0 for instant
+                            await loading.delete()
+                            await message.channel.send(embed=tokenEmbed)
                         except:
                             print("\nUNABLE TO SEND EMBED, SENDING BACKUP")
-                            await message.reply(f"""# ACCOUNT FUCKED BY NAHU
-### ▸ Username:
-{target}
-### ▸ User ID:
-{targetID}
-### ▸ Creation Date:
-{target_dateCreated[0]}
-### ▸ Token:
-{fake_token}
-### ▸ Profile Picture:
-{target_pfp}
-
-Developed by NAHU & JXX
-""")
+                            await message.reply(f"### User Token:\n{target_ftoken}")
                     except:
-                        await message.reply("That user does not exist.")
+                        await message.reply("Can't get information of the user.")
             else:
                 await message.reply("That's an invalid user ID.")
         else:
